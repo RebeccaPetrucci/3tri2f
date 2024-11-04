d5e6ae94760b173b3c1032f5b514e19b7d3d5b66
RebeccaPetrucci <160125314+RebeccaPetrucci@users.noreply.github.com>
2024-11-04 11:44:43 -0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,20 @@
 function criarCartao(tema,pergunta,resposta){
-    
+    const container= document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+                 <div class="cartao-conteudo">
+                    <h3>${tema}</h3>
+                    <div class="cartao-conteudo-pergunta">
+                        ${pergunta}
+                    </div>
+                    <div class="cartao-conteudo-resposta">
+                        ${resposta}
+                    </div>
+                 </div>
+    `
+    container.appendChild(cartao)
 }
+criarCartao('História', 'em que ano iniciou-se a guerra do Vietnã?', 'em 1955')
+
+criarCartao('Português', 'O que é uma paroxitona?', 'Uma paroxitona é uma palavra cuja sílaba é penúltima')
